4dabbfa9cf81f6d11aa291b2a213025b77d0ea54
Thanaporn19 <118186625+Thanaporn19@users.noreply.github.com>
2025-11-21 16:04:27 +0700

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,15 +1,34 @@
 $(document).ready(function(){
-  // mobile collapse closes on link click
+
+  // -------------------------------
+  // Navbar auto-close on mobile
+  // -------------------------------
   $('.navbar-collapse .nav-link').on('click', function(){ 
     $('.navbar-collapse').collapse('hide'); 
   });
 
-  // smooth scroll for anchors
+  // -------------------------------
+  // Smooth scroll for anchors
+  // -------------------------------
   $('a[href^="#"]').on('click', function(e){
     var target = $(this.hash);
     if(target.length){
       e.preventDefault();
-      $('html,body').animate({scrollTop: target.offset().top - 80}, 600);
+      $('html,body').animate({
+        scrollTop: target.offset().top - 80
+      }, 600);
     }
   });
+
+  // -------------------------------
+  // Header change color on scroll
+  // -------------------------------
+  $(window).on('scroll', function(){
+    if($(this).scrollTop() > 50){
+      $('.site-header').addClass('scrolled');
+    } else {
+      $('.site-header').removeClass('scrolled');
+    }
+  });
+
 });
